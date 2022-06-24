1c18dd1d48cfd07ad3b0e4f2f760d63cb5f826b9
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 11:22:14 +0530

    b13

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
-import { useRef, useState } from "react";
-import Hello from "./components/Hello";
-
 function App() {
-  let cityRef = useRef();
-  let descRef = useRef();
-  let [list, setList] = useState([]);
-
-  const addMoreRecord = () => {
-    let newRecord = {
-      city: cityRef.current.value,
-      desc: descRef.current.value,
-    };
-    let newList = [newRecord, ...list];
-    setList(newList);
-
-    cityRef.current.value = "";
-    descRef.current.value = "";
-  };
-
   return (
     <div>
-      <h1>Predefine Function By Browser!</h1>
-
-      <div>
-        <input
-          ref={cityRef}
-          type="text"
-          className="form-control form-control-lg my-1 "
-          placeholder="Enter City.."
-        />
-        <textarea
-          ref={descRef}
-          className="form-control form-control-lg my-1"
-          placeholder="Enter Desc"
-        />
-        <input
-          type="button"
-          className="btn btn-primary my-1 w-100"
-          value="Add More Record"
-          onClick={addMoreRecord}
-        />
-      </div>
-
-      {list.map((item, index) => (
-        <Hello item={item} key={index} />
-      ))}
+      <h1>Hello World</h1>
     </div>
   );
 }
