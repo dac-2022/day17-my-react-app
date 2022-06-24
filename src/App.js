679f4870e11eb345d75af40e65d490c9f8ff2e8b
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 09:23:21 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+// JSX
+// <App />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I am H1 TAG</h1>
+
+      <Hello />
     </div>
   );
+}
+
+// <Hello />
+function Hello() {
+  return <h1>I am also Tag! Hello JSX!</h1>;
 }
 
 export default App;
