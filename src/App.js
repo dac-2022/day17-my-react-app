6a2250d9672a528c3f851940bb7080ca739e7f22
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 11:30:30 +0530

    useEffect.....

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  // We are already initilize here
+  // Static Initilization
+  let [list, setList] = useState([]);
+
+  // Why shall i use ?
+  // Dymainc - Lets suppose coming from Server.
+  useEffect(() => {
+    console.log("Heloo");
+
+    // Initizaling the stateful variable from Server Data
+    setList([1, 1, 1, 1, 1, 1]);
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      {list.map(() => (
+        <h1>Hello World</h1>
+      ))}
     </div>
   );
 }
