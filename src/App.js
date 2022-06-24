7d265341ab4f4428a0a341e887ed7e945ed8185d
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 11:52:05 +0530

    clock.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  // We are already initilize here
-  // Static Initilization
-  let [list, setList] = useState([]);
+  let [secondCounter, setSecondCounter] = useState();
+  let [minuteCounter, setMinuteCounter] = useState();
+  let [hourCounter, setHourCounter] = useState();
 
-  // Why shall i use ?
-  // Dymainc - Lets suppose coming from Server.
   useEffect(() => {
-    console.log("Heloo");
+    // Initilzation
+    let date = new Date();
+    setSecondCounter(date.getSeconds());
+    setMinuteCounter(date.getMinutes());
+    setHourCounter(date.getHours());
 
-    // Initizaling the stateful variable from Server Data
-    setList([1, 1, 1, 1, 1, 1]);
-  }, []);
+    let clock = setTimeout(() => {
+      if (secondCounter == 60) {
+        setSecondCounter(0);
+      } else {
+        setSecondCounter(secondCounter + 1);
+      }
+    }, 1000);
+
+    // Un Intitialize
+    return () => clearTimeout(clock);
+  }, [secondCounter]);
 
   return (
     <div>
-      {list.map(() => (
-        <h1>Hello World</h1>
-      ))}
+      <h1>Clock Demo</h1>
+      <h1>
+        {hourCounter} : {minuteCounter} : {secondCounter}
+      </h1>
     </div>
   );
 }
