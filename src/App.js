fdd8654b0edf2d744c7d4625cd03f9a557358807
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 11:05:51 +0530

    dynmaic block.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Hello from "./components/Hello";
 
 function App() {
-  let list1 = [
-    { city: "delhi", desc: "Delhi desc.." },
-    { city: "kochi", desc: "kochi desc" },
-  ];
+  let [list, setList] = useState([{ city: "delhi", desc: "Delhi desc.." }]);
+
+  const addMoreRecord = () => {
+    let newRecord = { city: "TVM", desc: "TVM desc.." };
+    let newList = [newRecord, ...list];
+    setList(newList);
+  };
 
   return (
     <div>
       <h1>Predefine Function By Browser!</h1>
 
-      {list1.map((item, index) => (
+      <div>
+        <input type="button" value="Add More Record" onClick={addMoreRecord} />
+      </div>
+
+      {list.map((item, index) => (
         <Hello item={item} key={index} />
       ))}
     </div>
