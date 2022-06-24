4d02adaf83db6b4099806fea0b621ec04f269fd6
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 09:34:44 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
+import Hello from "./Hello";
+import World from "./World";
+
 function App() {
   return (
     <div>
       <h1>Predefine Function By Browser!</h1>
       <Hello />
       <World />
-      <Hello />
-      <World />
-    </div>
-  );
-}
-
-// <Hello />
-function Hello() {
-  return <h1>I am also Tag!</h1>;
-}
-
-function World() {
-  return (
-    <div>
-      <h1>Hello World Tag!</h1>
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+// export { Hello };
+export default Hello;
