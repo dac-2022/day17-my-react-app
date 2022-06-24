7b61c060eb38540c3479b7eb75f90db52ea46984
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-24 11:17:38 +0530

    b12

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,7 +2,10 @@ import World from "./World";
 
 function Hello({ item }) {
   return (
-    <div className="row">
+    <div className="row ">
+      <div className="col-12 col-md-6">
+        <World item={item} />
+      </div>
       <div className="col-12 col-md-6">
         <World item={item} />
       </div>
